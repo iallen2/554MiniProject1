f8c70c51657b3485d993cf031387dd1598940659
iallen2 <iallen2@wisc.edu>
2020-01-30 17:05:14 -0600

    add iocs and iorw to brg

--- baud_rate_generator.v
+++ baud_rate_generator.v
@@ -4,6 +4,8 @@ baud_rate_generator(
 			input clk, 
 			input [7:0]data_bus, 
 			input [1:0]ioaddr,
+			input iocs, 
+			input iorw, 
 			output enable);
 
 reg [15:0]final_count_up;
@@ -18,19 +20,22 @@ always @(posedge clk, posedge rst) begin
 	else if(final_count_up > baud_rate) begin
 		final_count_up = 0;
 	end
-	else begin
+	else if (iocs) begin
 		final_count_up = final_count_up + 1;
 	end
+	else begin
+		final_count_up = 0; 
+	end 
 end
 
 always @(posedge clk, posedge rst) begin 
 	if(rst) begin
 		baud_rate = 16'b0; 
 	end 
-	else if(ioaddr == 2'b10) begin // DB LOW
+	else if(ioaddr == 2'b10 & !iorw & iocs) begin // DB LOW
 		baud_rate = {{baud_rate[15:8]}, data_bus};
 	end 
-	else if(ioaddr == 2'b11) begin // DB HIGH
+	else if(ioaddr == 2'b11 & !iorw & iocs) begin // DB HIGH
 		baud_rate = {data_bus, {baud_rate[7:0]}};
 	end
 end
